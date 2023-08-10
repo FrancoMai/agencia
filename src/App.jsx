226e722016f7cddb5667b1f30067a08f0a01dcfb
Franco Mai <francoomai18@gmail.com>
2023-08-10 12:08:56 -0300

    primer comit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import NavBar from './NavBar/NavBar';
+
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    </>
+
+  )
+  
+}
+
+export default App
